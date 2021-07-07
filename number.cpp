0d2671f769e59a9d8426bb6cebad683029ce2882
Joelle Saurer <joelle.saurer@devoteam.com>
2021-07-07 16:37:19 +0200

    stashed changes

--- number.cpp
+++ number.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
   int x;
-  cout << "Type your favourite number: "; // Type a number and press enter
+  cout << "Type your  number: "; // Type a number and press enter
   cin >> x; // Get user input from the keyboard
-  cout << "Your favourite number is: " << x;
+  cout << "Your  number is: " << x;
   return 0;
 }
